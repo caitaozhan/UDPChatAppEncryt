f82ac9e1f9bd98c5f789ed50f8658464a258bf84
caitaozhan <caitaozhan@163.com>
2016-06-19 10:36:47 +0800

    完成alpha版本

--- DiffieHellman/DiffieHellman.java
+++
@@ -1,6 +0,0 @@
-package DiffieHellman;
-
-public class DiffieHellman
-{
-
-}
--- server/UDPCommunicationServer.java
+++ server/UDPCommunicationServer.java
@@ -1,5 +1,7 @@
 package server;
 
+import des.*;  // DES 算法
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -18,18 +20,19 @@ class CommunicationServer extends JFrame implements Runnable
 	private JPanel     panelNorth;
 	private JButton    rButton;      // 产生 R
 	private JButton    keyButton;    // 产生共享 key
-	private int p;      // 素数
-	private int g;      // p的原根
-	private int random; // 保密的随机数
-	private String r1;  // 对方的R1（假设我是Bob）
-	private int K;      // 双方共享的秘密密钥
+	private int p;            // 素数
+	private int g;            // p的原根
+	private int random;       // 保密的随机数
+	private String r1;        // 对方的R1（假设我是Bob）
+	private String sharedKey; // 双方共享的密钥
 	Thread s;
-	private DatagramSocket sendSocket, receiveSocket;  // 用于收发UDP数据报
+	private DatagramSocket datagramSocket;             // 用于收发UDP数据报
 	private DatagramPacket sendPacket, receivePacket;  // 包含具体的要传输的信息
 	// 为了发送数据，要将数据封装到DatagramPacket中，使用DatagramSocket发送该包
 	private SocketAddress sendAddress;
 	private String  name;
 	private boolean canSend;
+	private byte[] ESCEOT;  // 帧定界符
 
 	public CommunicationServer()
 	{
@@ -63,8 +66,8 @@ class CommunicationServer extends JFrame implements Runnable
 			random = (int) (Math.random()*p);
 		}while(random <= 1);
 
-		setSize(400, 400);
-		setTitle("UDPServer");
+		setBounds(400, 100, 400, 400);
+		setTitle("UDPServer Copyright 詹才韬");
 		add(panelNorth, BorderLayout.NORTH);
 		add(textArea, BorderLayout.CENTER);
 		add(jTextFieldInput, BorderLayout.SOUTH);
@@ -77,15 +80,21 @@ class CommunicationServer extends JFrame implements Runnable
 		});
 		try
 		{
-			sendSocket = new DatagramSocket();         // 创建发送方的套接字，IP默认为本地，端口号随机
-			//System.out.println(sendSocket.getPort());// 为什么是 -1 ?
-			receiveSocket = new DatagramSocket(8002);  // 创建接收方的套接字，IP(chosen by the kernal),端口号8002
+			datagramSocket = new DatagramSocket(8002);      // 创建接收方的套接字，IP(chosen by the kernal),端口号8002
+			//System.out.println(datagramSocket.getPort()); // 为什么是 -1 ?
 		}
 		catch (SocketException e)
 		{
 			e.printStackTrace();
 			System.exit(1);
 		}
+		
+		ESCEOT = new byte[4];
+		ESCEOT[0] = 2;
+		ESCEOT[1] = 7;
+		ESCEOT[2] = 0;
+		ESCEOT[3] = 3;
+		
 		s = new Thread(this);  //创建线程
 		s.start();
 	}
@@ -98,27 +107,53 @@ class CommunicationServer extends JFrame implements Runnable
 			{
 				byte buf[] = new byte[1024];
 				receivePacket = new DatagramPacket(buf, buf.length);  // 可以不是每一次都new么？用同一个
-
-				receiveSocket.receive(receivePacket);  // 通过套接字，等待接受数据
-				canSend = true;                        // 必须先受到客户端的消息，我方（服务器）才能够发送消息（给客户端）
-				
+				datagramSocket.receive(receivePacket);  // 通过套接字，等待接受数据
+				canSend = true;                         // 必须先受到客户端的消息，我方（服务器）才能够发送消息（给客户端）
 				sendAddress = receivePacket.getSocketAddress();
-				
 				name = receivePacket.getAddress().toString().trim();
-				textArea.append("\n来自主机:" + name + "\n端口:" + receivePacket.getPort());
-				textArea.append("\n客户端: ");
-				byte[] data = receivePacket.getData();
-				String receivedString = new String(data);
-				textArea.append(receivedString);
+				textArea.append("\n来自主机:" + name + " 端口:" + receivePacket.getPort());
+				byte[] databyte = receivePacket.getData();
 				
-				if(jTextFieldInput.isEditable() == false)  // 最开始计算共享密钥的时候，jTextFieldInput 无法编辑
+				if(jTextFieldInput.isEditable() == true)   // 当jTextFieldInput可以编辑的时候，可以发送信息，此时才进行加密
 				{
-					r1 = receivedString.trim();
+					textArea.append("\n客户端密文是：" + new String(databyte) + '\n');
+					
+					byte[] databyteEOT = new byte[databyte.length];
+					copyByteArray(databyteEOT, databyte);
+					
+					int indexEOT = FindByteArray.findByteArray(databyteEOT, ESCEOT);  // 找到在哪里填空了字节
+					
+					if(indexEOT != -1)                     // 把帧定界符去掉
+					{// 减去字节填充
+						databyte = new byte[indexEOT];
+						for(int i = 0; i < indexEOT; i++)
+						{
+							databyte[i] = databyteEOT[i];
+						}
+					}
+					else
+					{
+						throw new Exception("没有找到 EOT");
+					}
+					
+					databyte = DES.decrypt(databyte, sharedKey);   
+					String receivedString = new String(databyte);
+					textArea.append("明文是：" + receivedString + '\n');
+				}
+				if(jTextFieldInput.isEditable() == false)  // 当jTextFieldInput无法编辑的时候(初始阶段), 接受的是共享密钥
+				{
+					r1 = new String(databyte);
+					r1 = r1.trim();                        // 把多余的空格删除掉
+					textArea.append("\n客户端的R1 = " + r1);
 				}
 			}
-			catch (IOException e)
+			catch (IOException ioe)
 			{
-				textArea.append("网络通信出现错误,问题在于" + e.toString());
+				textArea.append("网络通信出现错误,问题在于" + ioe.toString());
+			}
+			catch (Exception e)
+			{
+				textArea.append("解密出现异常");
 			}
 		}
 	}
@@ -141,8 +176,22 @@ class CommunicationServer extends JFrame implements Runnable
 				textArea.append(string);
 				byte[] databyte = string.getBytes();
 				
-				sendPacket = new DatagramPacket(databyte, databyte.length, sendAddress);
-				sendSocket.send(sendPacket);
+				databyte = DES.encrypt(databyte, sharedKey);  
+				
+				//增加字节填充: 两个字符ESC,EOT（4个字节长度）
+				int length = databyte.length + 4;
+				byte[] databyteEND = new byte[length];
+				for(int i = 0; i < databyte.length; i++)
+				{
+					databyteEND[i] = databyte[i];
+				}
+				databyteEND[length - 4] = 2;
+				databyteEND[length - 3] = 7;  // ESC: 27
+				databyteEND[length - 2] = 0;
+				databyteEND[length - 1] = 3;  // EOT: 03
+				
+				sendPacket = new DatagramPacket(databyteEND, databyteEND.length, sendAddress);
+				datagramSocket.send(sendPacket);
 				
 				canSend = false;  // 恢复为“不能发送”的状态，等待客户端发送下一个消息
 			}
@@ -165,7 +214,7 @@ class CommunicationServer extends JFrame implements Runnable
 	{
 		if(rButton == null)  // 当第一次调用这个方法的时候，rButton == null，进行初始化操作
 		{
-			rButton = new JButton("产生 R");
+			rButton = new JButton("产生并发送R2");
 			rButton.addActionListener(new ActionListener()
 			{
 				@Override
@@ -177,11 +226,11 @@ class CommunicationServer extends JFrame implements Runnable
 						{
 							String G = String.valueOf(g);
 							String R2 = modularExponentiation(G);  // 假设我是Bob，产生 R2
-							textArea.append("\n产生R=" + R2);
+							textArea.append("\n服务器端R2 = " + R2);
 							byte[] databyte = R2.getBytes();
 							
 							sendPacket = new DatagramPacket(databyte, databyte.length, sendAddress);
-							sendSocket.send(sendPacket);           // 发送 R2
+							datagramSocket.send(sendPacket);       // 发送 R2
 							
 							canSend = false;  // 恢复为“不能发送”的状态，等待客户端发送下一个消息
 							rButton.setVisible(false);   // “产生R”的按钮消失
@@ -219,14 +268,13 @@ class CommunicationServer extends JFrame implements Runnable
 				{
 					try
 					{
-						System.out.println(r1);
-						String key = modularExponentiation(r1);  // 产生共享的key
-						K = Integer.parseInt(key);
-						textArea.append("\n共享密钥是: " + key);
-						
-						canSend = false;  // 恢复为“不能发送”的状态，等待客户端发送下一个消息
+						String key = modularExponentiation(r1);       // 产生sharedKey（不是8位）
+						sharedKey = NormalizeToEight.normalize(key);  // 规格化成为8位的sharedKey
+						textArea.append("\n共享密钥是: " + sharedKey + "\n产生共享密钥使用了 Diffie-Hellman-Caitao算法\n");
+						textArea.append("加密算法使用了 DES算法\n-----------请放心，以下通信是经过加密的！------------\n");
+						canSend = false;                         // 恢复为“不能发送”的状态，等待客户端发送下一个消息
 						keyButton.setVisible(false);             // “产生共享key”按钮消失 
-						jTextFieldInput.setEnabled(true);        // 输入栏可以编辑
+						jTextFieldInput.setEditable(true);       // 输入栏可以编辑
 					}
 					catch (NumberFormatException nfe)
 					{
@@ -250,6 +298,23 @@ class CommunicationServer extends JFrame implements Runnable
 		tmp = tmp.mod(BigInteger.valueOf(p));    // tmp = tmp%p
 		return tmp.toString();
 	}
+	
+	/*
+	 * 把array2 复制给array1
+	 * 复制成功返回true
+	 * 复制失败返回false
+	 */
+	public boolean copyByteArray(byte[] array1, byte[] array2)
+	{
+		if(array1.length != array2.length)
+			return false;
+		
+		for(int i = 0; i < array1.length; i++)
+		{
+			array1[i] = array2[i];
+		}
+		return true;
+	}
 }
 
 public class UDPCommunicationServer
@@ -258,11 +323,5 @@ public class UDPCommunicationServer
 	{
 		CommunicationServer UDPserver = new CommunicationServer();
 		UDPserver.setVisible(true);
-		
-		/*String base = "10";
-		String key = UDPserver.modularExponentiation(base);
-		System.out.println(key);*/
-		
-		
 	}
 }
